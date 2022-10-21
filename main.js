6fff43713db6772eddf18c2ee819e7564f9fa447
juan <jjhuertasbotache@gmail.com>
2022-10-21 06:28:14 -0500

    barra de busqueda con busqueda por titulo funcional

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var listObjectsTasks = [];
 var title = document.getElementById("taskTitle");
 var description = document.getElementById("taskDescription");
 var date = document.getElementById("taskDate");
+var idTasks = 0;
 
 
 window.onload=function(){
@@ -17,8 +18,13 @@ window.onload=function(){
         else{document.getElementById("alert").style.display = "none";}
     });
     
-    var search = document.getElementById("search").addEventListener("click",()=>{
-        search("title","10-10-2022",true);
+    var searchbtn = document.getElementById("search").addEventListener("click",()=>{
+        let title = document.getElementById("searchInput").value
+        console.log("looking for: ",title);
+        search(title,"10-10-2022",true);
+    });
+    var clearbtn = document.getElementById("clear").addEventListener("click",()=>{
+        clear();
     });
     
 }
@@ -43,6 +49,7 @@ function generateTask(title,description,date) {
         description : description,
         date : date,
     }
+
     let keys = Object.keys(task);
     for (let x = 0; x < keys.length; x++) {
         const value = task[keys[x]];
@@ -64,11 +71,14 @@ function generateTask(title,description,date) {
     htmlTask.classList.add("list-group-item","d-flex");
     htmlCheckbox.classList.add("form-check-input","mx-2");htmlCheckbox.type="checkbox";    
     htmlInner.classList.add("task_container");
+
     htmlTitle.classList.add("ms-2", "fw-bold");
     htmlTitle.innerHTML = task.title;
-    // htmlDescription.classList.add("");
-    htmlDate.classList.add("ms-2");
+    
+    htmlDate.classList.add("ms-5");
     htmlDate.innerHTML = "Due date: " + task.date;
+    
+    htmlDescription.classList.add("ms-3");
     htmlDescription.innerHTML = task.description;
     
     htmlTask.appendChild(htmlCheckbox);
@@ -79,16 +89,40 @@ function generateTask(title,description,date) {
     
     document.getElementById("tasks").appendChild(htmlTask);
     listObjectsTasks.push(task);
+    idTasks++;
     successful = true;
     return(successful);
 }
 
 /**
  * Takes info and filter the tasks by the parameters given
- * @param {str} string 
+ * @param {str} title 
  * @param {int} date 
  * @param {boolean} done 
  */
-function search(string,date,done) {
-    let x = document.getElementsByTagName("li").style.display = "none";
+function search(title,date,done) {
+    let taskList = Array.from(document.getElementsByTagName("li"))
+    for (let z = 0; z < taskList.length; z++) {
+        const element = taskList[z];
+        let taskTitle = Array.from(element.getElementsByClassName("fw-bold"));taskTitle=taskTitle[0].innerHTML;
+        let taskDescription = Array.from(element.getElementsByClassName("ms-3"));taskDescription=taskDescription[0].innerHTML;
+        let taskDate = Array.from(element.getElementsByClassName("ms-5"));taskDate=taskDate[0].innerHTML;
+        console.log(taskTitle,taskDescription,taskDate);
+
+        if (title.toLowerCase() != taskTitle.toLowerCase()) {
+            alert(taskTitle);
+            taskList.splice(z,1)
+            element.style.setProperty('display', 'none', 'important');
+        }
+    }
+    taskList.forEach(element => {
+        element.style.setProperty('display', 'flex', 'important');
+    });
+}
+/**
+ * clears the existing filters
+ */
+function clear() {   
+    document.getElementById("dropdown1").value = 0;
+    document.getElementById("checkbox").checked = false;
 }
